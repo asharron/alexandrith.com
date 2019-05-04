06fd4c4ddba3951cc1229ef3a2c8de9c8af4a77e
asharron <alexsharron@gmail.com>
2019-05-04 11:01:52 -0400

    Converting to python server

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import Header from './components/Header';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Header>This worked!</Header>
+        );
+    }
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <nav className="navbar navbar-light border border-dark rounded">
+        <a className="navbar-brand" href="#">Alexandrith Sharron</a>
+    </nav>
+);
+
+export default Header;
